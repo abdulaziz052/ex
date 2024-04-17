1d5e9025f63580b1511cdf6e10a5a92138630892
abdulaziz052 <killerabdulaziz60@gmail.com>
2024-04-17 17:59:28 +0500

    add

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,22 @@
+let plus = document.querySelector('.plus')
+let minus = document.querySelector('.minus')
+let count = document.querySelector('.count')
 let images = document.querySelectorAll('.item__img')
 let item__img = document.querySelector('.item')
-let tabs = document.querySelectorAll('.tab__btn')
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
 
-
-tabs.forEach((tab, index) => {
+tabs.forEach(tab => {
     tab.addEventListener('click', () => {
-        tabs.forEach(tab => { tab.classList.remove('active') });
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
         tab.classList.add('active')
+        target.classList.add('active')
     })
 })
 images.forEach(item => {
@@ -14,4 +24,26 @@ images.forEach(item => {
         item__img.src = item.src
     })
 })
+plus.addEventListener('click', () =>{
+    count.innerText = Number(count.innerText) + 1
+})
+minus.addEventListener('click' , () =>{
+    count.innerText = Number(count.innerText) - 1
+})
+if (count.innerText = 0) {
+    
+}
+
+
+// let tabs = document.querySelectorAll('.tab__btn')
+// let all_content = document.querySelectorAll('.content')
+
+
+// tabs.forEach((tab, index) => {
+//     tab.addEventListener('click', () => {
+//         tabs.forEach(tab => { tab.classList.remove('active') });    
+//         tab.classList.add('active')
+//     })
+// })
+
 
